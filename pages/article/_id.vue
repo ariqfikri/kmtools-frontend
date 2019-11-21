c65b898ed889e9d804bf119d367307a3c49014e9
ariqfikri <lolicodesu@gmail.com>
2019-11-21 22:07:59 +0700

    [add] detail article

---
+++ pages/article/_id.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <baca-artikel
+      :title="article.title"
+      :article="article.content"
+    ></baca-artikel>
+  </v-app>
+</template>
+<script>
+export default {
+  /* eslint-disable */
+  data() {
+    return {
+      article: []
+    }
+  },
+  beforeMount() {
+    console.log(this.$route.params.id)
+    this.$axios
+      .get('/article/'+ this.$route.params.id)
+      .then((response) => {
+        console.log(response.data.data)
+        this.article = response.data.data
+      })
+      .catch(function(error) {
+        console.log(error)
+      })
+  }
+}
+</script>
